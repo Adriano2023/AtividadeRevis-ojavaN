864c459e86f3ac0035bf6f61f65554af27095f9f
Adriano2023 <dirtjump278@gmail.com>
2023-03-20 22:01:44 -0300

    20/02/2023

--- App.java
+++ App.java
@@ -2,6 +2,7 @@ package AtividadeRevisão;
 
 public class App {
     public static void main(String[] args) {
-        
+        ExemploRevisão obj= new ExemploRevisão();
+        obj.exemplo2();
     }
 }
--- ExemploRevisão.java
+++ ExemploRevisão.java
@@ -1,5 +1,34 @@
 package AtividadeRevisão;
 
-public class ExemploRevisão  {
-    
+import java.util.Scanner;
+
+import javax.print.event.PrintEvent;
+
+public class ExemploRevisão {
+    Scanner sc = new Scanner(System.in);
+
+    public void exemplo1 (){
+        System.out.println("Digite o numero");
+        double ndigitando = sc.nextDouble();
+         if (ndigitando > 10) {
+             System.out.println("e maior que dez");
+
+         }
+         else{
+           System.out.println("e menor que dez");
+         }
+        }
+          public void exemplo2(){
+         System.out.println("informe a quantidade de horas trabalhadas no mes");
+         int horasTrabalhadas = sc.nextInt();
+         System.out.println("Informe o valor da hora");
+         double ValorHoras = sc.nextDouble();
+         int HorasExtra = horasTrabalhadas - 200;
+         double salarioTotal = horasTrabalhadas + HorasExtra * ValorHoras * 50 / 100;
+         System.out.println("O valor das Horas é R$" +salarioTotal);
+         
+
+         }
+       
+
 }
